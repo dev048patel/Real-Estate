7a52156cbf9754edfc39f5e9476cc3e70e1c3635
Dev Patel <p.p.dev2004@gmail.com>
2025-11-04 13:26:46 -0600

    Appwrite Database Set up

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -6,7 +6,11 @@ export const config = {
     platform : 'com.dev.real-estate',
     endpoint : process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
     projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
-
+    databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
+    galleriesCollectionId: process.env.EXPO_PUBLIC_APPWRITE_GALLERIES_COLLECTION_ID,
+    reviewsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_REVIEWS_COLLECTION_ID,
+    agentsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_AGENTS_COLLECTION_ID,
+    propertiesCollectionId: process.env.EXPO_PUBLIC_APPWRITE_PROPERTIES_COLLECTION_ID,
 }
 
 export const client = new Client();
@@ -20,7 +24,8 @@ client
 export const avatar = new Avatars(client);
 // Create new user account
 export const account = new Account(client);
-
+// Database 
+export const databases = new Databases(client);
 
 // implementing functionality to authenticate user
 export async function login(){
