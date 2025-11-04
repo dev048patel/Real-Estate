1f833a592e78524445e53f32ffe2ceb825e0ace9
Dev Patel <p.p.dev2004@gmail.com>
2025-11-04 14:03:16 -0600

    Database Seed

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -4,8 +4,9 @@ import Search from "@/Components/Search";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
 import { useGlobalContext } from "@/lib/globalprovider";
+import seed from "@/lib/seed";
 import { Link } from "expo-router";
-import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import { Button, FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
@@ -17,6 +18,7 @@ export default function Index() {
   return (
     <SafeAreaView className="bg-white h-full">
       
+      <Button title="Seed" onPress={seed } />
       {/* FlatList */}
       <FlatList 
         data ={[1, 2, 3, 4]} 
@@ -66,7 +68,7 @@ export default function Index() {
              {/* Another FlatList */}
              {/* Featured Card with horizontal  */}
              <FlatList 
-                data={[1, 2, 3, 4]}
+                data={[5,6,7]}
                 renderItem={(item) => <FeaturedCard />} // Rendering Featured Card
                 keyExtractor={(item) => item.toString()} // to convert number to string
                 horizontal // to make it horizontal
