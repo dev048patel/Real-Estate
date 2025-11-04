eb4fd988dbc3879268194b1f857b25c041770dbd
Dev Patel <p.p.dev2004@gmail.com>
2025-11-04 14:20:15 -0600

    Fetch Query

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -18,7 +18,6 @@ export default function Index() {
   return (
     <SafeAreaView className="bg-white h-full">
       
-      <Button title="Seed" onPress={seed } />
       {/* FlatList */}
       <FlatList 
         data ={[1, 2, 3, 4]} 
